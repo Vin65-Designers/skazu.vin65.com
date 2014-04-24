3f2628c94bba15840c0b56ecab903fa0f28dd683
Lucas Klaassen <lucas@vin65.com>
2014-04-24 11:18:06 -0700

    Initial Deploy Files - Before Q/A

--- www/assets/js/scripts.js
+++ www/assets/js/scripts.js
@@ -5,6 +5,7 @@
 			v65.global.addToCartListener();
 			v65.global.continueShopping();
 			v65.global.mainMenuHover();
+			v65.global.v65MMenu.mobileMenu.init();
 		},
 		addButtonListener : function(){
 			if(document.addEventListener){
@@ -25,6 +26,82 @@
 			$(".mainMenu ul li ul li").hover(function(){
 				$(this).parent().parent().children("a").toggleClass("hover");
 			});
+		},
+		//Responsive Desktop-Tablet-Mobile Menu
+		v65MMenu : {
+			settings : {
+				userTools: "#user-tools",
+				menuIcon: ".menuIcon",
+				mainNav: ".mainMenu",
+				bodyContainer: "body",
+				tabletWrapper: ".tabletWrapper"
+			},
+			mobileMenu : {
+				init: function(){
+					// Check if mobile
+					window.mobilecheck = function() {
+						var check = false;
+						(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+						return check; 
+					}
+					if(window.mobilecheck()){
+						//If Mobile setup initial DOM structure
+						$(v65.global.v65MMenu.settings.userTools).prepend($(v65.global.v65MMenu.settings.menuIcon));
+						$(v65.global.v65MMenu.settings.tabletWrapper).prepend($(v65.global.v65MMenu.settings.mainNav));
+						$(v65.global.v65MMenu.settings.mainNav).height($(window).height() - 48);
+						//Init animation tracking
+						v65.global.v65MMenu.mobileMenu.menuAnimation();
+					} else {
+						//Methods for tablet Devices
+						$(v65.global.v65MMenu.settings.menuIcon).toggle(function() {
+						  $(v65.global.v65MMenu.settings.mainNav).show();
+						}, function() {
+						  $(v65.global.v65MMenu.settings.mainNav).hide();
+						});
+					}
+				},
+				menuAnimation: function() {
+					//Animation Tracking
+					( function( $ ) {
+					$(v65.global.v65MMenu.settings.menuIcon).add($("body > *").not("body > .menuIcon")).on( 'touchstart click', function(e) {
+					  var $body = $(v65.global.v65MMenu.settings.bodyContainer),
+					      $menu = $(v65.global.v65MMenu.settings.mainNav),
+					      transitionEnd = 'transitionend webkitTransitionEnd otransitionend MSTransitionEnd';
+					  // When animating begins add classes
+					  if($(this).is($('.menuIcon'))) {
+					  	e.preventDefault();
+					  	$menu.addClass( 'animating' );
+					  	if ( $menu.hasClass( 'menu-visible' ) ) {
+					  	 $menu.addClass( 'rightAnimate' );
+					  	 $body.css("position", "relative");
+					  	} else {
+					  	 $menu.addClass( 'leftAnimate' );
+					  	 $body.css("position", "fixed");
+					  	}
+						  //When transition is over remove animating classes
+						  $menu.on( transitionEnd, function() {
+						   $menu
+						    .removeClass( 'animating leftAnimate rightAnimate' )
+						    .toggleClass( 'menu-visible' );
+						   $menu.off( transitionEnd );
+						  });
+						  // When user touches outside of the mobile menu, close it
+						} else if($menu.hasClass( 'menu-visible' ) && !$(this).is($('.tabletWrapper')) && !$(this).is($('.mainMenu'))) {
+							e.preventDefault();
+							$menu.addClass( 'animating' );
+							$menu.addClass( 'rightAnimate' );
+							$body.css("position", "relative");
+							$menu.on( transitionEnd, function() {
+						   $menu
+						    .removeClass( 'animating leftAnimate rightAnimate' )
+						    .removeClass( 'menu-visible' );
+						   $menu.off( transitionEnd );
+						  });
+						}
+					});
+					} )( jQuery );
+				}
+			}
 		}
 	},
 	cookies : {
@@ -72,16 +149,8 @@
 		initPhotoGallery : function(){
 			if($("#pagePhotoGallery").length){
 				$("#pagePhotoGallery").v65PhotoGallery({
-					/*
-						Uncomment the code below if you want to change how the photo gallery is displayed.
-
-						galleryHeight : 420, // This value is translated to 420px and will change the photogallery height
-						galleryWidth : 630, // This value is translated to 630px and will change the photogallery width
-						pauseTime : 5000, // Adjust how long the image is displayed for. Value is in milliseconds
-						animSpeed : 1000, // Adjust the transition speed between images. Value is in milliseconds
-						controlNav : false, // hide the 1,2,3 navigation
-						directionNav : false // hide the arrow navigation
-					*/
+						galleryHeight : null, // This value is translated to 420px and will change the photogallery height
+						galleryWidth : null // This value is translated to 630px and will change the photogallery width
 				});
 			}
 		},
